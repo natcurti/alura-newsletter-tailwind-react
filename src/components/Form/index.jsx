f963820275b706e03169798eb0e9d2b70b172d99
natcurti <naticurti01@gmail.com>
2024-03-17 13:00:36 -0300

    Use tailwind directives

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -17,10 +17,7 @@ const Form = ({ onSubmit }) => {
       <Input inputType="text" placeholderInput="Insira seu nome" />
       <Input inputType="email" placeholderInput="Insira seu email" />
 
-      <button
-        type="submit"
-        className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black "
-      >
+      <button type="submit" className="alura-button">
         Seguir
       </button>
     </form>
