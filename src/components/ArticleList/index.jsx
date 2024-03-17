95aff9ce3015f26f2ffd84e76f69ecd8fb679a39
natcurti <naticurti01@gmail.com>
2024-03-17 11:21:54 -0300

    Fetch data from json and create article component

--- src/components/ArticleList/index.jsx
+++ src/components/ArticleList/index.jsx
@@ -1,10 +1,11 @@
 import data from "../../../articles.json";
+import Article from "../Article";
 
 const ArticleList = () => {
   return (
-    <div>
-      {data.map((article, index) => (
-        <p key={index}>{article.title}</p>
+    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-5xl sm:grid-cols-2 lg:grid-cols-3">
+      {data.map((props, index) => (
+        <Article key={index} {...props} />
       ))}
     </div>
   );
